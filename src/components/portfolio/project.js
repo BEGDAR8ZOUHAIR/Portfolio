ec56b9fe32a46cda620fa3e5d7242fb4cb9391c1
BEGDAR8ZOUHAIR <begdar8zouhair@gmail.com>
2023-09-11 21:38:13 +0100

    fix bug

--- src/components/portfolio/project.js
+++ src/components/portfolio/project.js
@@ -11,6 +11,43 @@ const Project = [
       "React Native-based mobile app for iOS that helps 🐕 owners search for 🐕 parks, save their preferred ones, obtain directions from their current location to the selected 🐕 park, and access information on the distance between two locations, as well as an estimated travel time.",
     link: "",
   },
+  {
+    id: 1,
+    image: Project1,
+    title: "Pooch Parks",
+    category: "Mobile App",
+    description:
+      "React Native-based mobile app for iOS that helps 🐕 owners search for 🐕 parks, save their preferred ones, obtain directions from their current location to the selected 🐕 park, and access information on the distance between two locations, as well as an estimated travel time.",
+    link: "",
+  },
+  {
+    id: 1,
+    image: Project1,
+    title: "Pooch Parks",
+    category: "Mobile App",
+    description:
+      "React Native-based mobile app for iOS that helps 🐕 owners search for 🐕 parks, save their preferred ones, obtain directions from their current location to the selected 🐕 park, and access information on the distance between two locations, as well as an estimated travel time.",
+    link: "",
+  },
+  {
+    id: 1,
+    image: Project1,
+    title: "Pooch Parks",
+    category: "Mobile App",
+    description:
+      "React Native-based mobile app for iOS that helps 🐕 owners search for 🐕 parks, save their preferred ones, obtain directions from their current location to the selected 🐕 park, and access information on the distance between two locations, as well as an estimated travel time.",
+    link: "",
+  },
+  {
+    id: 1,
+    image: Project1,
+    title: "Pooch Parks",
+    category: "Mobile App",
+    description:
+      "React Native-based mobile app for iOS that helps 🐕 owners search for 🐕 parks, save their preferred ones, obtain directions from their current location to the selected 🐕 park, and access information on the distance between two locations, as well as an estimated travel time.",
+    link: "",
+  },
+
  
 ];
 
